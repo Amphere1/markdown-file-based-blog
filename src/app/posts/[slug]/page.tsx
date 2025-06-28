35f13713cb7c6ce5acedb806b90398947f27c253
Krishnakant Sahu <krishnakantsahu916@gmail.com>
2025-06-28 20:56:58 +0530

    created dynamic post page

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,29 @@
+import { getPostData } from "@/lib/posts";
+import { notFound } from "next/navigation";
+
+type props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function PostPage({ params }: props) {
+  const {slug} = params;
+
+  try {
+    const post = await getPostData(slug);
+
+    return (
+      <article className="prose prose-neutral dark:prose-invert mx-auto px-4">
+        <h1>{post.title}</h1>
+        <p className="text-sm text-gray-500">
+          By {post.author} · {post.date}
+        </p>
+        <div dangerouslySetInnerHTML={{ __html: post.contentHtml }} />
+      </article>
+    );
+  } catch (error) {
+    console.log(error);
+    notFound();
+  }
+}
