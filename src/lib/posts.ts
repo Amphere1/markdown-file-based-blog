1584cdc352323c2e5eb05171e5c548b4193178cc
Krishnakant Sahu <krishnakantsahu916@gmail.com>
2025-06-28 20:49:38 +0530

    created dynamic route file

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
 
 const postDirectory = path.join(process.cwd(), "posts");
 
@@ -33,9 +35,33 @@ export function getSortedPostsData(): postMetaData[] {
     };
   });
 
-  allPostsData.sort((a,b) => {
+  allPostsData.sort((a, b) => {
     return new Date(b.date).getTime() - new Date(a.date).getTime();
   });
 
   return allPostsData;
+}
+
+export type FullPostData = postMetaData & {
+  contentHtml: string;
+} 
+
+export async function getPostData(slug: string): Promise<FullPostData> {
+  const fullPath = path.join(postDirectory, `${slug},md`);
+
+  const fileContents = fs.readFileSync(fullPath, "utf-8");
+
+  const { data, content } = matter(fileContents);
+
+  const processedContent = await remark().use(html).process(content);
+  const contentHtml = processedContent.toString();
+
+  return {
+    title: data.title,
+    date: data.date,
+    author: data.author,
+    description: data.description,
+    slug,
+    contentHtml,
+  };
 }
